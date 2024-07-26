35c3943e7ce17ce3c5cdbddb965dfeb9463ce4fb
Eric Kao <erickao@mail.apex.com.tw>
2024-07-26 15:53:59 +0800

    Webform使用ADO.NET連接資料庫

---
+++ WebForm_ADO.NET/DatabaseHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace WebForm_ADO.NET
+{
+    public class DatabaseHelper
+    {
+        private string connectionString;
+
+        public DatabaseHelper()
+        {
+            connectionString = WebConfigurationManager.ConnectionStrings["dbConnStr"].ConnectionString;
+        }
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataTable);
+
+                }catch(Exception ex) 
+                { 
+                    throw new Exception("Database query failed", ex);
+                }
+            }
+
+            return dataTable;
+        }
+    }
+}
---
+++ WebForm_ADO.NET/WebForm_DbConn.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.WebSockets;
+
+namespace WebForm_ADO.NET
+{
+    public partial class WebForm_DbConn : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DataTable queryResult = FindAll();
+                BindGrid(GridView1,queryResult);
+            }
+        }
+
+        protected DataTable FindAll()
+        {
+            string query = "SELECT TOP(100) * FROM dbo.Customers";
+            DatabaseHelper dbHelper = new DatabaseHelper();
+            DataTable dataTable = dbHelper.ExecuteQuery(query);
+
+            return dataTable;
+        }
+
+        private void BindGrid(GridView gridView,object data)
+        {
+            gridView.DataSource = data;
+            gridView.DataBind();
+        }
+    }
+}
